6ed959cf097db3a37787a6608b9f8071123dab05
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-17 14:34:18 +0200

    Update main.tsx

--- frontend/dev/main.tsx
+++ frontend/dev/main.tsx
@@ -1,15 +1,3 @@
 import { bootstrapBpmnTool } from '../src/main';
-import UrlModel from '../src/extensions/url-model';
-import UrlPropertiesProvider from '../src/extensions/url-properties-provider';
 
-bootstrapBpmnTool({
-  modelerOptions: {
-    extensions: UrlModel,
-    providers: [
-      {
-        priority: 500,
-        instance: UrlPropertiesProvider,
-      },
-    ],
-  },
-});
+bootstrapBpmnTool();
