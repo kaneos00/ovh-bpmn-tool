dd0f60e16307dfee9fab470dbb169d890a403c6d
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-18 11:58:33 +0200

    Update url-properties-provider.ts

--- frontend/src/extensions/url-properties-provider.ts
+++ frontend/src/extensions/url-properties-provider.ts
@@ -39,30 +39,48 @@ function UrlEntry(props: any) {
   const debounce = useService('debounceInput');
 
   /*
-  const getValue = () => {
-   return element.businessObject.$attrs?.['url:link'] || '';
-  };
-  */
-  const getValue = () => {
-  console.log('URL DEBUG', {
-      element,
-      businessObject: element.businessObject,
-      attrs: element.businessObject?.$attrs,
-      urlLink: element.businessObject?.['url:link'],
-      type: element.businessObject?.$type,
-      model: element.businessObject?.$model,
-    });
-  
-    return element.businessObject?.['url:link'] || '';
-  };
+   * ANCIEN CODE
+   * ----------
+   * La propriété XML était recherchée directement avec son nom
+   * préfixé "url:link". Or, dans bpmn-moddle, la propriété JavaScript
+   * correspondante est "link".
+   *
+   * const getValue = () => {
+   *   return element.businessObject.$attrs?.['url:link'] || '';
+   * };
+   *
+   * Ancien code de debug :
+   *
+   * const getValue = () => {
+   *   console.log('URL DEBUG', {
+   *     element,
+   *     businessObject: element.businessObject,
+   *     attrs: element.businessObject?.$attrs,
+   *     urlLink: element.businessObject?.['url:link'],
+   *     type: element.businessObject?.$type,
+   *     model: element.businessObject?.$model,
+   *   });
+   *
+   *   return element.businessObject?.['url:link'] || '';
+   * };
+   *
+   * const setValue = (value: string) => {
+   *   return modeling.updateProperties(element, {
+   *     'url:link': value || undefined,
+   *   });
+   * };
+   */
 
   /*
-  const setValue = (value: string) => {
-    return modeling.updateProperties(element, {
-      'url:link': value || undefined,
-    });
+   * NOUVEAU CODE
+   * ------------
+   * En JavaScript, bpmn-moddle expose la propriété sous le nom "link".
+   * Le préfixe "url:" est utilisé lors de la sérialisation XML.
+   */
+  const getValue = () => {
+    return element.businessObject?.link || '';
   };
-  */
+
   const setValue = (value: string) => {
     return modeling.updateProperties(element, {
       link: value || undefined,
