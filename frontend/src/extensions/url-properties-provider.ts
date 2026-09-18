103ad900c2c5b7c638f811ac81e3c648a5b89cd0
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-18 12:20:27 +0200

    Update url-properties-provider.ts

--- frontend/src/extensions/url-properties-provider.ts
+++ frontend/src/extensions/url-properties-provider.ts
@@ -2,29 +2,47 @@ import { is } from 'bpmn-js/lib/util/ModelUtil';
 
 import {
   TextFieldEntry,
+  isTextFieldEntryEdited
+} from '@bpmn-io/properties-panel';
+
+import { useService } from 'bpmn-js-properties-panel';
+
+
+/*
+============================================================
+ANCIEN CODE
+============================================================
+
+import { is } from 'bpmn-js/lib/util/ModelUtil';
+
+import {
+  TextFieldEntry,
   isTextFieldEntryEdited,
 } from '@bpmn-io/properties-panel';
 
 import { useService } from 'bpmn-js-properties-panel';
 
 class UrlPropertiesProvider {
+
   getGroups(element: any) {
+
     return (groups: any[]) => {
-      if (!is(element, 'bpmn:BaseElement')) {
+
+      if (!is(element, 'bpmn:Task')) {
         return groups;
       }
 
       groups.push({
         id: 'url',
-        label: 'URL',
+        label: 'Procédure',
         entries: [
           {
-            id: 'url-link',
+            id: 'procedureUrl',
             element,
-            component: UrlEntry,
-            isEdited: isTextFieldEntryEdited,
-          },
-        ],
+            component: UrlField,
+            isEdited: isTextFieldEntryEdited
+          }
+        ]
       });
 
       return groups;
@@ -32,69 +50,121 @@ class UrlPropertiesProvider {
   }
 }
 
-function UrlEntry(props: any) {
+
+function UrlField(props: any) {
+
   const { element, id } = props;
 
   const modeling = useService('modeling');
-  const debounce = useService('debounceInput');
 
-  /*
-   * ANCIEN CODE
-   * ----------
-   * La propriété XML était recherchée directement avec son nom
-   * préfixé "url:link". Or, dans bpmn-moddle, la propriété JavaScript
-   * correspondante est "link".
-   *
-   * const getValue = () => {
-   *   return element.businessObject.$attrs?.['url:link'] || '';
-   * };
-   *
-   * Ancien code de debug :
-   *
-   * const getValue = () => {
-   *   console.log('URL DEBUG', {
-   *     element,
-   *     businessObject: element.businessObject,
-   *     attrs: element.businessObject?.$attrs,
-   *     urlLink: element.businessObject?.['url:link'],
-   *     type: element.businessObject?.$type,
-   *     model: element.businessObject?.$model,
-   *   });
-   *
-   *   return element.businessObject?.['url:link'] || '';
-   * };
-   *
-   * const setValue = (value: string) => {
-   *   return modeling.updateProperties(element, {
-   *     'url:link': value || undefined,
-   *   });
-   * };
-   */
-
-  /*
-   * NOUVEAU CODE
-   * ------------
-   * En JavaScript, bpmn-moddle expose la propriété sous le nom "link".
-   * Le préfixe "url:" est utilisé lors de la sérialisation XML.
-   */
-  const getValue = () => {
-    return element.businessObject?.link || '';
-  };
+  const value =
+    element.businessObject.get('wiki:procedureUrl') || '';
 
   const setValue = (value: string) => {
-    return modeling.updateProperties(element, {
-      link: value || undefined,
+
+    modeling.updateProperties(element, {
+      'wiki:procedureUrl': value
     });
+
   };
 
-  return TextFieldEntry({
-    element,
-    id,
-    label: 'URL',
-    getValue,
-    setValue,
-    debounce,
-  });
+  return (
+    <TextFieldEntry
+      id={id}
+      element={element}
+      label="Procedure URL"
+      getValue={() => value}
+      setValue={setValue}
+    />
+  );
 }
+
+
+export default UrlPropertiesProvider;
+
+============================================================
+FIN ANCIEN CODE
+============================================================
+*/
+
+
+class UrlPropertiesProvider {
+
+  getGroups(element: any) {
+
+    return (groups: any[]) => {
+
+      if (!is(element, 'bpmn:Task')) {
+        return groups;
+      }
+
+      groups.push({
+        id: 'url',
+        label: 'Procédure',
+        entries: [
+          {
+            id: 'procedureUrl',
+            element,
+            component: UrlField,
+            isEdited: isTextFieldEntryEdited
+          }
+        ]
+      });
+
+      return groups;
+    };
+  }
+}
+
+
+function UrlField(props: any) {
+
+  const { element, id } = props;
+
+  const modeling = useService('modeling');
+
+  const value =
+    element.businessObject.get('wiki:procedureUrl') || '';
+
+  const setValue = (value: string) => {
+
+    modeling.updateProperties(element, {
+      'wiki:procedureUrl': value
+    });
+
+  };
+
+  return (
+    <div>
+
+      <TextFieldEntry
+        id={id}
+        element={element}
+        label="Procedure URL"
+        getValue={() => value}
+        setValue={setValue}
+      />
+
+      {value && (
+        <div style={{ marginTop: '8px' }}>
+
+          <a
+            href={value}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              cursor: 'pointer'
+            }}
+          >
+            Ouvrir la procédure ↗
+          </a>
+
+        </div>
+      )}
+
+    </div>
+  );
+}
+
 
 export default UrlPropertiesProvider;
