4752134b19d19d90df205279673a9d9583efde7a
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-18 14:18:00 +0200

    Update url-properties-provider.ts

--- frontend/src/extensions/url-properties-provider.ts
+++ frontend/src/extensions/url-properties-provider.ts
@@ -78,13 +78,12 @@ function UrlEntry(props: any) {
   */
   const getValue = () => {
     console.log('URL DEBUG', {
-        element,
-        businessObject: element.businessObject,
-        attrs: element.businessObject?.$attrs,
-        urlLink: element.businessObject?.['url:link'],
-      });
+      element,
+      businessObject: element.businessObject,
+      link: element.businessObject?.link,
+    });
   
-    return element.businessObject?.['url:link'] || '';
+    return element.businessObject?.link || '';
   };
 
 
