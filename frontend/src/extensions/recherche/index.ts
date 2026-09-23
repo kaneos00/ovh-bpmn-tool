8871d9efa064ad45ac2f2460ca521e5bbb02cda1
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-23 19:22:44 +0200

    feat(recherche): add Recherche extension foundation

---
+++ frontend/src/extensions/recherche/index.ts
@@ -0,0 +1,7 @@
+export { RechercheService } from './recherche-service';
+export type {
+  RechercheProvider,
+  RechercheResult,
+} from './recherche-service';
+
+export { default as RechercheModule } from './recherche-module';
