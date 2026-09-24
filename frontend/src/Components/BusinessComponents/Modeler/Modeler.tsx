2add21a80aba2242b1bea75d44a6707afc595efe
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-24 14:05:30 +0200

    feat: load minimap styles

--- frontend/src/Components/BusinessComponents/Modeler/Modeler.tsx
+++ frontend/src/Components/BusinessComponents/Modeler/Modeler.tsx
@@ -3,6 +3,7 @@ import React, { FC, RefObject } from 'react';
 import 'camunda-bpmn-js/dist/assets/camunda-platform-modeler.css';
 import 'bpmn-js-color-picker/colors/color-picker.css';
 import 'bpmn-js-embedded-comments/assets/comments.css';
+import 'diagram-js-minimap/assets/diagram-js-minimap.css';
 import './Modeler.css';
 
 type ModelerProps = {
