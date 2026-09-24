4ad976b7636581818c6e95f5eff0620fece7e6b3
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-24 15:48:42 +0200

    fix: avoid duplicate properties provider registration

--- frontend/src/shared/hooks/useModelerInstance.ts
+++ frontend/src/shared/hooks/useModelerInstance.ts
@@ -5,30 +5,18 @@ import BpmnViewer from 'camunda-bpmn-js/lib/camunda-platform/NavigatedViewer';
 import { useBpmnToolOptions } from '../../Providers/BpmnToolOptions/useBpmnToolOptions';
 
 export const useModelerInstance = () => {
-  const {
-    getModelerExtensions,
-    getModelerModules,
-    getModelerProviders,
-    getModelerLinting,
-  } = useBpmnToolOptions();
+  const { getModelerExtensions, getModelerModules, getModelerLinting } =
+    useBpmnToolOptions();
 
   const lintingOptions = getModelerLinting();
 
   const modeler = useMemo(() => {
-    const instance = new BpmnModeler({
+    return new BpmnModeler({
       keyboard: { bindTo: document },
       additionalModules: getModelerModules(),
       ...(lintingOptions.active ? { linting: lintingOptions } : {}),
       moddleExtensions: getModelerExtensions(),
     });
-
-    const propertiesPanel = instance.get('propertiesPanel') as any;
-
-    getModelerProviders().forEach(({ priority = 500, instance: Provider }) => {
-      propertiesPanel.registerProvider(priority, new Provider());
-    });
-
-    return instance;
   }, []);
 
   const getModelerInstance = () => {
