a5debc1a036570c833ad867e4587e5a2ba146861
kaneos00 <120753823+kaneos00@users.noreply.github.com>
2026-09-25 10:41:07 +0200

    feat: support resource drag and drop moves

--- backend/src/modules/ResourceContext/domain/Resource/Resource.ts
+++ backend/src/modules/ResourceContext/domain/Resource/Resource.ts
@@ -23,76 +23,37 @@ export class Resource {
     this.internalId = id || new Uuid();
   }
 
-  get id(): Uuid {
-    return this.internalId;
-  }
+  get id(): Uuid { return this.internalId; }
+  get name(): string { return this.state.name.value; }
+  get description(): Nullable<string> { return this.state.description; }
+  get depth(): number { return this.state.depth || 0; }
+  get parentId(): Nullable<string> { return this.state.parentId || null; }
+  get type(): ResourceTypeEnum { return this.state.type; }
+  get createdBy(): string { return this.state.createdBy; }
+  get authToken(): Nullable<string> { return this.state.authToken || null; }
+  get state(): ResourceProps { return this._stateProps; }
 
-  get name(): string {
-    return this.state.name.value;
-  }
+  public restore() { return this.state; }
 
-  get description(): Nullable<string> {
-    return this.state.description;
-  }
-
-  get depth(): number {
-    return this.state.depth || 0;
-  }
-
-  get parentId(): Nullable<string> {
-    return this.state.parentId || null;
-  }
-
-  get type(): ResourceTypeEnum {
-    return this.state.type;
-  }
-
-  get createdBy(): string {
-    return this.state.createdBy;
-  }
-
-  get authToken(): Nullable<string> {
-    return this.state.authToken || null;
-  }
-
-  get state(): ResourceProps {
-    return this._stateProps;
-  }
-
-  // Used as mapper when we will have ValueObjects
-  public restore() {
-    return this.state;
-  }
-
-  public move(parentDepth?: number) {
+  public move(parentId: Nullable<string>, parentDepth?: number) {
+    this.state.parentId = parentId;
     this.state.depth = Resource.computeDepth(parentDepth);
   }
 
-  public rename(name: string) {
-    this.state.name.value = name;
-  }
-
-  public changeDescription(description: string) {
-    this.state.description = description;
-  }
-
-  public updateAuthToken(authToken: string) {
-    this.state.authToken = authToken;
-  }
+  public rename(name: string) { this.state.name.value = name; }
+  public changeDescription(description: string) { this.state.description = description; }
+  public updateAuthToken(authToken: string) { this.state.authToken = authToken; }
 
   static apply(props: ResourceProps, id?: Uuid): Resource {
-    return new Resource(
-      {
-        name: props.name,
-        description: props.description,
-        parentId: props.parentId || null,
-        depth: props.depth,
-        type: props.type,
-        createdBy: props.createdBy,
-        authToken: props.authToken,
-      },
-      id,
-    );
+    return new Resource({
+      name: props.name,
+      description: props.description,
+      parentId: props.parentId || null,
+      depth: props.depth,
+      type: props.type,
+      createdBy: props.createdBy,
+      authToken: props.authToken,
+    }, id);
   }
 
   static computeDepth(parentDepth?: number) {
